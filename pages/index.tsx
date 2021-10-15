ff4e165a912d36c51dddaf4cea22562d5e769ab9
KIYO1234 <getstarted3601@gmail.com>
2021-10-15 15:14:02 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>KYOKUMEMO</h1>;
+  return (
+    <>
+      <h1>KYOKUMEMO</h1>
+      <div>develop</div>
+    </>
+  );
 };
 
 export default Home;
